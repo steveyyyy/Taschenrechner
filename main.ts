25c190449674d7dda2c0453c436936f5a429a414
steveyyyy <46565994+steveyyyy@users.noreply.github.com>
2022-11-10 15:33:31 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    zahl = zahl + 1
+    basic.showNumber(zahl)
+})
+input.onButtonPressed(Button.AB, function () {
+    zahl = 0
+    basic.showNumber(zahl)
+})
+input.onButtonPressed(Button.B, function () {
+    zahl = zahl - 1
+    basic.showNumber(zahl)
+})
+let zahl = 0
+zahl = 0
+basic.showNumber(zahl)
+basic.forever(function () {
+	
+})
